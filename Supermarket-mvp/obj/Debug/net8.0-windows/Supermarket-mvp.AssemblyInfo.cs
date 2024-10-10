6d4eae9a16b44a157cf500daec5fda86d385a9dc
Juan De La Cruz <delacruzjuan07@hotmail.com>
2024-10-09 23:41:04 -0500

    Se realizo implementacion de productRepository

--- Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2531f0cf88aeb8dca851e4815b09a8b88acba5a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2eed2d4043eb958414e7319dd62d2e1bd1613dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
