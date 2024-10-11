f1ac2761169ea65b4b38749098a13a627a9927cd
Juan De La Cruz <delacruzjuan07@hotmail.com>
2024-10-10 22:25:51 -0500

    Agregada bd de products

--- Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+369057517efa99d5ceace7e22b2efc3fc128d12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4fe81a185096c7b429d2e64aaab09ddafdc9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
